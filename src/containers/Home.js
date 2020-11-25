c73b3a264027b397936fc3cf909dc443d3fbdf5f
Md Alamin <mdalamin.eu@gmail.com>
2020-11-25 03:29:41 +0200

    update something

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,23 +7,12 @@ class Home extends Component {
     return (
       <div>
         <CenteredHeader>
-           Hi 👋, I'm Karim A Full-stack Software Engineer. based in Helsinki,Finland 🇫🇮.
+          Hey👋! I'm Md alamin a javaScript developer based in Helisinki.
         </CenteredHeader>
         <p>
-          I'm currently 🎯 on <strong>Node.js</strong>, <strong>TypeScript</strong>,
-          <strong>React</strong>, <strong>Redux</strong>, <strong>Python</strong>, <strong>Next JS</strong>,
-          <strong>NoSQL</strong>, <strong>PostgreSQL</strong>,  <strong>AWS</strong>, and <strong>GraphQL</strong>. Also keeping
-          👀 on  <strong>React-native</strong>, <strong>Docker</strong>,<strong>Kubernetes</strong>, and
-             <strong>Cloud-native ecosystem</strong> with strong interest.
-        </p>
-        <p>
-        I have been working professionally as a software engineer for 3 years + now. In these years I have helped to create scalable, maintainable, and impactful products in high-volume and fast-paced businesses.
-        </p>
-        <p>
-          ​Outside of code, I like to create in other ways. From photography to
-          travel, long driving , I'm always trying new ways of expressing
-          myself. I also like to meditate, talk to strangers, and run lifestyle
-          experiments.
+        <strong>Currently working on side projects. In my spare time, I enjoy learning new things 📚, building personal-projects 🔨, 
+        doing photography 🥊, contributing to Open Source 🖥️ , and watching horror 🎞️. </strong>
+        
         </p>
       </div>
     );
