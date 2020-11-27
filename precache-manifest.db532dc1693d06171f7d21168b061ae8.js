ba5b56a4359f6559d37cc63e3eebf0dbaa44c351
Md Alamin <mdalamin.eu@gmail.com>
2020-11-27 02:13:42 +0200

    Updates

---
+++ precache-manifest.db532dc1693d06171f7d21168b061ae8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "c44d780c69381341b3f3",
+    "url": "/mdalamin-eu/Portfolio/static/css/main.44e9336d.chunk.css"
+  },
+  {
+    "revision": "c44d780c69381341b3f3",
+    "url": "/mdalamin-eu/Portfolio/static/js/main.c44d780c.chunk.js"
+  },
+  {
+    "revision": "32345bc641163d38321e",
+    "url": "/mdalamin-eu/Portfolio/static/css/1.c6a2f600.chunk.css"
+  },
+  {
+    "revision": "32345bc641163d38321e",
+    "url": "/mdalamin-eu/Portfolio/static/js/1.32345bc6.chunk.js"
+  },
+  {
+    "revision": "d0c803fa66cd5e75ec22",
+    "url": "/mdalamin-eu/Portfolio/static/js/runtime~main.d0c803fa.js"
+  },
+  {
+    "revision": "ace7c38f8daa59852ffd1ba1390ad676",
+    "url": "/mdalamin-eu/Portfolio/static/media/alamin.ace7c38f.png"
+  },
+  {
+    "revision": "1823e8d953b9556f81b456819d2c2bfa",
+    "url": "/mdalamin-eu/Portfolio/static/media/dev.1823e8d9.png"
+  },
+  {
+    "revision": "9538b00a90f5606200552bf364d9cdfb",
+    "url": "/mdalamin-eu/Portfolio/static/media/Breed.9538b00a.png"
+  },
+  {
+    "revision": "533dc006afc89ae4b7b3a494cc00b647",
+    "url": "/mdalamin-eu/Portfolio/static/media/Robo.533dc006.png"
+  },
+  {
+    "revision": "4bd7f3ec0a68db6cad0107d0635b3071",
+    "url": "/mdalamin-eu/Portfolio/static/media/desk.4bd7f3ec.jpg"
+  },
+  {
+    "revision": "26d7c5bfe270eec14e2e93f64de7a96d",
+    "url": "/mdalamin-eu/Portfolio/static/media/photography.26d7c5bf.jpg"
+  },
+  {
+    "revision": "f92a690f3b63553a51e8b51da6e5cb59",
+    "url": "/mdalamin-eu/Portfolio/static/media/Dubai.f92a690f.JPG"
+  },
+  {
+    "revision": "4b3ca47008dd85c80555bfd7fcf3bd84",
+    "url": "/mdalamin-eu/Portfolio/static/media/Travel.4b3ca470.jpg"
+  },
+  {
+    "revision": "3f7a93127b19f8c2de9acd033be6cef4",
+    "url": "/mdalamin-eu/Portfolio/static/media/travel2.3f7a9312.JPG"
+  },
+  {
+    "revision": "ac779bbbbecb7f41e65056093e585334",
+    "url": "/mdalamin-eu/Portfolio/static/media/Photo.ac779bbb.jpg"
+  },
+  {
+    "revision": "c8952d7dbcaa8d24e5430f33230d0f49",
+    "url": "/mdalamin-eu/Portfolio/static/media/football.c8952d7d.JPG"
+  },
+  {
+    "revision": "27175e4123f9ef54aa008bf330f25a09",
+    "url": "/mdalamin-eu/Portfolio/static/media/uni.27175e41.JPG"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/mdalamin-eu/Portfolio/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/mdalamin-eu/Portfolio/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/mdalamin-eu/Portfolio/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/mdalamin-eu/Portfolio/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/mdalamin-eu/Portfolio/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "1405370f02fc477aec986a064fd78e75",
+    "url": "/mdalamin-eu/Portfolio/index.html"
+  }
+];
